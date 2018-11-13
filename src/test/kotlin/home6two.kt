106bba45284baaba48b39d288cee5785131612df
Marina <44848727+m-shek@users.noreply.github.com>
2018-11-13 16:33:31 +0600

    Update home6two.kt

--- src/test/kotlin/home6two.kt
+++ src/test/kotlin/home6two.kt
@@ -1,27 +1,12 @@
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.DescribeSpec
 
-fun binarv1 (c:String, d:String) :String {
-    var e:MutableList<String> = mutableListOf()
-    for (i in 0..c.length-1) {
-        when {
-            ((c.get(i) == '0') and (d.get(i) == '0')) -> e.add(i, "0")
-            ((c.get(i) == '0') and (d.get(i) == '1')) -> e.add(i,"1")
-            ((c.get(i) == '1') and (d.get(i) == '0')) -> e.add(i,"1")
-            ((c.get(i) == '1') and (d.get(i) == '1')) -> e.add(i,"10")
-        }
-    }
-    var sr_res=""
-    println("$e")
-
-    for (j in e.lastIndex downTo  0){
-        sr_res= e[j]+sr_res
-    }
-
-    var sr1 = sr_res
-    return sr1
+fun binarv1 (c:String, d:String):String{
+    val c1=c.toInt(2)
+    val d1=d.toInt(2)
+    val sum:String =(c1.plus(d1).toString(2))
+    return sum
 }
-
 
 
 class binar:DescribeSpec ({
@@ -42,7 +27,7 @@ class binar:DescribeSpec ({
 
                 binarv1("111","100") shouldBe ("1011")
                 binarv1("011","100") shouldBe ("111")
-                 binarv1("010","010") shouldBe ("100")
+                binarv1("010","010") shouldBe ("100")
                 binarv1("001","001") shouldBe ("010")
             }
         }
